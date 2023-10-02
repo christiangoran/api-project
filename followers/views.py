cf0213225123c6bfa6b9f15ed3f815849c5fe3c2
christiangoran <christian.goran@protonmail.com>
2023-10-02 13:45:49 +0200

    Refactored code in posts and profiles views.py to generic

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from followers.models import Followers
+from .serializers import FollowersSerializer
 
-# Create your views here.
+
+class FollowersList(generics.ListCreateAPIView):
+    """
+    List all followers, i.e. all instances of a user
+    following another user'.
+    Create a follower, i.e. follow a user if logged in.
+    Perform_create: associate the current logged in user with a follower.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = FollowersSerializer
+    queryset = Followers.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a follower
+    No Update view, as we either follow or unfollow users
+    Destroy a follower, i.e. unfollow someone if owner
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = FollowersSerializer
+    queryset = Followers.objects.all()

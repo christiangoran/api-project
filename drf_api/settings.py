fa6944b093f6fee3c6eedb05746bb3f97dea8e9c
christiangoran <christian.goran@protonmail.com>
2023-10-05 18:20:23 +0200

    Updated settings.py

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -61,17 +61,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'taskmanager-heroku.herokuapp.com']
 
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN'),
-        os.environ.get('CLIENT_ORIGIN_DEV')
+if "CLIENT_ORIGIN" in os.environ:
+    CORS_ALLOWED_ORIGINS = [os.environ.get("CLIENT_ORIGIN")]
+if "CLIENT_ORIGIN_DEV" in os.environ:
+    extracted_url = re.match(
+        r"^.+-", os.environ.get("CLIENT_ORIGIN_DEV", ""), re.IGNORECASE
+    ).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
-
-CSRF_TRUSTED_ORIGINS = [os.environ.get(
-    'CLIENT_ORIGIN_DEV',
-    'CLIENT_ORIGIN',
-)]
-
 
 # Application definition
 
